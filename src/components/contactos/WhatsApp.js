243aec537d2baf045ceabd2478ba5549aa821094
elifred2022 <elifed21@hotmail.com>
2023-08-09 19:49:12 -0300

    update mensaje de ws

--- src/components/contactos/WhatsApp.js
+++ src/components/contactos/WhatsApp.js
@@ -6,7 +6,21 @@ export const WhatsAppButton = () => {
   return (
     <div>
       <a
-        href="https://wa.me/+5491127003907?text=Dejame tu comentario y sugerencias en breve te contestare"
+        href="https://wa.me/+5491127003907?text=Hola buen día para todos,  
+        recuerda que antendemos en nuestro local AV ARIETA 2688 LOCAL 9 SAN JUSTO De 13 a 19hrs.
+                
+        Tenemos las zonas de deliverys, zona oeste; Ramos Mejia, San Justo, Haedo, Moron y Castelar.
+
+        pedidos al mayor sin costo, a todas las zonas CABA y GBA
+        
+        Realiza tu pedido.
+
+        Formas de pago
+        Efectivo,
+        transferencia al alias: boka23, 
+        Todo pago electrónico sera aceptado una vez verificado su efectividad en cuenta.
+        
+        Estamos para servirte🤗"
         target="_blank"
       >
         <FloatingWhatsApp />
